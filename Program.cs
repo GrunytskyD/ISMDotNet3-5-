d508d446c933f2b1342ba60d7381cb75d17891c8
Dmytro Grunytsky <dima.grunitsky@gmail.com>
2020-10-20 17:08:12 +0300

    Сума індексів додатних елементів

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //сума елементів масива А із 100 цілих чисел
+            int A[100];
+            int suma;//змінна,що містить суму
+            int i;//додаткова змінна
+            //Введення масива А
+            // ...
+            // знаходження суми 
+            suma = 0;
+            for (i = 0; i < 100; i++)
+                suma += A[i];
+        }
+    }
+}
